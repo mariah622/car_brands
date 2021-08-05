2c0b3b3c664517991636addb9f1142cffa6475be
Mariah Morales <mjustine622@gmail.com>
2021-08-04 22:31:48 -0400

    added some validations

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,6 +13,7 @@ class CarsController < ApplicationController
     
     def create 
         @car = Car.new(car_params)     
+        byebug
         if @car.save
             redirect_to cars_path
         else
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,14 @@
 class Car < ApplicationRecord
     # belongs_to :brand
 
+    validates :color, :condition, presence: true, length: {minimum: 2}  
+    validates :name, presence: true, uniqueness: {scope: :year, message: "Sorry that Car and Year already exists"}
+    validates :price, numericality: true 
+
     def name_and_year
         "#{self.name} - #{self.year}"
     end
 end
+
+
+# uniqueness: {scope: year, message: "Tha
