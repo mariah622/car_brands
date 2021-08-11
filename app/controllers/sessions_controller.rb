487493d83ec9ea275c732a5707c178f1fc2679ff
Mariah Morales <mjustine622@gmail.com>
2021-08-10 23:22:05 -0400

    finished sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,30 @@ class SessionsController < ApplicationController
     end 
 
     def create
+        user = User.find_by_email(params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to shoes_path
+        else
+            flash[:message] = "Invalid credentials. Please try again. "
+            redirect_to login_path
+        end
     end 
 
     def omniauth
+        # User.find_or_create_by(name: params["name"])
         user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u| 
             u.email = auth['info']['email']
             u.username = auth['info']['name']
             u.password = SecureRandom.hex(15)
-        end 
-
-        if user.valid?
-            session.delete(:user_id)
-            flash[:message] = 'Successful Login' 
+        end
+        
+        if user.valid? 
+            session[:user_id] = user.id
+            flash[:message] = "Successful Login!!"
             redirect_to brands_path
         else
-        end 
+        end
         #email
         #password
         #username
@@ -38,5 +47,3 @@ class SessionsController < ApplicationController
         
 
 end 
-
-# request.env['omniauth.auth']
